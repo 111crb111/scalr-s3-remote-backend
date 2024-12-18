ab7b74a1038bc81ef9bacbd24eeb221b63fa655e
Yaroslav Subota <111CRB111@gmail.com>
2024-12-18 14:14:15 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,6 +5,7 @@ variable "region" {
 
 variable "bucket_name" {
     description = "The name of the S3 bucket to create"
+    default     = "crb-s3"
 }
 
 resource "random_string" "bucket_suffix" {
@@ -58,16 +59,6 @@ resource "aws_dynamodb_table" "terraform_locks" {
   tags = {
     Name        = "TerraformLockTable"
     Environment = "dev"
-  }
-}
-
-terraform {
-  backend "s3" {
-    bucket         = "alfiia-terraform-state-bucket"
-    key            = "global/s3/terraform.tfstate"  # Path in S3 where the state file will be stored
-    region         = "us-west-2"
-    encrypt        = true                           # Enables server-side encryption
-    dynamodb_table = "terraform-locks"      # Name of the DynamoDB table for state locking
   }
 }
 
